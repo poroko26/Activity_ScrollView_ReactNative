51916a31c434fdfebf6e6e2c09bea8fa70d786d1
poroko26 <arnoldjamesfelis@gmail.com>
2023-11-11 22:08:16 +0800

    Initial commit

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView } from 'react-native-web';
+import data from './src/list';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.txt1}>LIST OF GUEST</Text>
+      {data.map((datas)=>{
+        return(
+          <View>
+            <ScrollView>
+              <Text style={styles.txt1}>{datas.name}</Text>
+              <Text style={styles.txt2}>{datas.place}</Text>
+            </ScrollView>
+          </View>
+        )
+      })}
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +26,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    gap: 20,
+    marginTop: 100,
+    marginLeft: 30
+  },
+  txt1: {
+    fontWeight: 'bold',
+    fontSize: 18,
+
   },
 });
---
+++ src/list.js
@@ -0,0 +1,84 @@
+const data = [
+  {
+    id: 0,
+    name: "JAGUNOS, JOANA",
+    place: "Balilihan, Bohol"
+},
+{
+    id: 1,
+    name: "TAHIL, ADRIAN JANE",
+    place: "Balilihan, Bohol"
+},
+{
+    id: 2,
+    name: "GARCIA, RODEL",
+    place: "Balilihan, Bohol"
+},
+{
+    id: 3,
+    name: "PAGUICAN, NICOLE JAMES",
+    place: "Jagna, Bohol"
+},
+{
+    id: 4,
+    name: "FELISILDA, ARNOLD JAMES",
+    place: "Valencia, Bohol"
+},
+{
+    id: 5,
+    name: "SARABIA, EARL MIKE",
+    place: "Tagbilaran, Bohol"
+},
+{
+    id: 6,
+    name: "CARBONILLA, MARLON",
+    place: "San Isidro, Bohol"
+},
+{
+    id: 7,
+    name: "JUNTILLA, HERSHIEL JAY",
+    place: "Sevilla, Bohol"
+},
+{
+  id: 8,
+  name: "EUSALAN, DAISY DIANE",
+  place: "Dauis, Bohol"
+},
+{
+  id: 9,
+  name: "ESCUADRO, NELBREY JILLIAN",
+  place: "Cortes, Bohol"
+},
+{
+  id: 10,
+  name: "MADANGUIT, RUSEL NINO",
+  place: "Cortes, Bohol"
+},
+{
+  id: 11,
+  name: "AMORA, KENT IVAN",
+  place: "Cortes, Bohol"
+},
+{
+  id: 12,
+  name: "BONGCARAS, AIREEN MAE",
+  place: "Dauis, Bohol"
+},
+{
+  id: 13,
+  name: "TABANYAG, JOHN",
+  place: "Cortes, Bohol"
+},
+{
+  id: 14,
+  name: "ANTOPINA, DEBORAH",
+  place: "Tagbilaran, Bohol"
+},
+{
+  id: 15,
+  name: "MIRABITE, MIC-ROLAND",
+  place: "Maribojoc, Bohol"
+},
+];
+
+export default data;
